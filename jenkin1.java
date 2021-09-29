20f8b86209959baebb3150c518eca035c1c40136
Ballem Samhitha <54273046+samhitha8@users.noreply.github.com>
2021-09-29 11:36:10 +0530

    Add files via upload

---
+++ jenkin1.java
@@ -0,0 +1,9 @@
+import java.lang;
+public class jenkin1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello samhitha! Welcome to the world of jenkins. Happy learning!");
+	}
+
+}
